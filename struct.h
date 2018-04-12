6b95975fe00d30761f2ac111da7779236f60780e
johnvictorfs <37747572+johnvictorfs@users.noreply.github.com>
2018-04-12 15:24:25 -0300

    Add files via upload

---
+++ struct.h
@@ -0,0 +1,18 @@
+struct Character {
+	char nome[50];
+	int moedas;
+	int hp;
+	int defesa;
+	int ataque;
+	int loot;
+	int hit;
+	int lootraro;
+	char arma[50];
+	char escudo[50];
+};
+
+	struct Character user; // Declaração Usuário
+	struct Character goblin; // Declaração goblin
+	struct Character aranha; // Declaração aranha gigante
+	struct Character esqueleto; // Declaração esqueleto
+
